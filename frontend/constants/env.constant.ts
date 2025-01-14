14fcf0f76d7c853be12a409b86a37fc2784f6d71
Hrishabh Thakur <hrishabhthakur2@gmail.com>
2025-01-14 11:08:01 +0530

    fourth commit

--- frontend/constants/env.constant.ts
+++ frontend/constants/env.constant.ts
@@ -1,5 +1,5 @@
 const env = {
-  API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5050',
+  API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5051',
   SOCKET_SERVER_URL: process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || 'http://localhost:5051',
 };
 
